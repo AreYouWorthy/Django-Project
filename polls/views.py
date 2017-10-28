12afe40fcc8ecc0764dd2ebaa44c44945c9b56e3
AreYouWorthy <wongben16@gmail.com>
2017-10-28 14:38:02 -0400

    Before deletion of redundant code

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+	conext = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list': lastest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
+	output = ', '.join([q.question_text for q in lastest_question_list])
+	return HttpResponse(output)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
