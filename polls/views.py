d331ecf2b82ee0b28a23b816b234465292164b99
AreYouWorthy <wongben16@gmail.com>
2017-10-28 14:39:25 -0400

    After deletion of redundent code

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
 # Create your views here.
 
@@ -8,13 +7,7 @@ def index(request):
 	lastest_question_list = Question.objects.order_by('-pub_date')[:5]
 	conext = {'latest_question_list': latest_question_list}
 	return render(request, 'polls/index.html', context)
-	template = loader.get_template('polls/index.html')
-	context = {
-		'latest_question_list': lastest_question_list,
-	}
-	return HttpResponse(template.render(context, request))
-	output = ', '.join([q.question_text for q in lastest_question_list])
-	return HttpResponse(output)
+
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
