ee8b847811685f5a9f05177adcb99e1b3a0aff83
AreYouWorthy <wongben16@gmail.com>
2017-10-28 16:03:16 -0400

    Finished Part3

--- polls/views.py
+++ polls/views.py
@@ -11,14 +11,6 @@ def index(request):
 	context = {'latest_question_list': latest_question_list}
 	return render(request, 'polls/index.html', context)
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
-
 def detail(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html' , {'question':question})
